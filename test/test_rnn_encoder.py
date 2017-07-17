1053cc6df0a0727593806eee6aeda0bd6cd65437
Solumilken <s916526000@gmail.com>
2017-07-17 12:51:19 +0800

    change input name and make mask as tensor

--- test/test_rnn_encoder.py
+++ test/test_rnn_encoder.py
@@ -8,15 +8,15 @@ class RNNEncoderTest(tf.test.TestCase):
 
     def test_lstm_context_encoder(self):
         with self.test_session() as sess:
-            input_with_embedding = tf.constant(
+            single_context_with_embedding = tf.constant(
                 [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
-                 [[1, 2, 3, 4, 5], [11, 12, 13, 14, 15],
-                     [4, 1, 2, 6, 7], [0, 0, 0, 0, 0]],
-                 [[10, 20, 30, 40, 50], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]])
-            input_with_embedding = tf.cast(input_with_embedding, tf.float32)
+                 [[1, 2, 3, 4, 5], [11, 12, 13, 14, 15], [4, 1, 2, 6, 7], [0, 0, 0, 0, 0]],
+                 [[10, 20, 30, 40, 50], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
+                 [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]])
+            single_context_with_embedding = tf.cast(single_context_with_embedding, tf.float32)
             context_vector = lstm_context_encoder(
-                input_with_embedding=input_with_embedding,
-                mask=np.array([2, 3, 1]),
+                input_with_embedding=single_context_with_embedding,
+                mask=tf.constant([2, 3, 1, 0]),
                 utterence_level_state_size=50,
                 utterence_level_keep_proba=0.9,
                 utterence_level_num_layers=2,
